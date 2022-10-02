de8c23c5f8195c06f3134839395ec1ae90729c03
Sushant kumar jha <72967077+sushant135@users.noreply.github.com>
2022-10-02 16:13:24 +0530

    Create Wrapper.java

---
+++ Wrapper.java
@@ -0,0 +1,10 @@
+class test{
+    public static void main(String [] args){
+
+    Integer i=400, j=400;
+    if(i==j)
+    System.out.println("same");
+    else
+    System.out.println("not same");
+}
+}
